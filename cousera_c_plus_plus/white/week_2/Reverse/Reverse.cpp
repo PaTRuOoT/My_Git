9ee88f00b51e94bc23abd749f29591a851e3f18d
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-20 15:18:56 +0300

    simple test is OK

--- cousera_c_plus_plus/white/week_2/Reverse/Reverse.cpp
+++ cousera_c_plus_plus/white/week_2/Reverse/Reverse.cpp
@@ -5,9 +5,14 @@
 using namespace std;
 
 void Reverse(vector<int>& v) {
-    
+    vector<int> temp;
+    for (int i = v.size() - 1; i >= 0; --i) {
+        temp.push_back(v[i]);
+    }
+    v = temp;
 }
 
 int main () {
-    return 0;
+    vector<int> numbers = {1, 5, 3, 4, 2};
+    Reverse(numbers);
 }
