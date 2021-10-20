5a56a659d816e0d8725f07e77cfcb26fff221690
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-20 13:34:16 +0300

    Del test-1

--- cousera_c_plus_plus/white/week_2/pieriemieshchieniie_strok/MoveStrings.cpp
+++ cousera_c_plus_plus/white/week_2/pieriemieshchieniie_strok/MoveStrings.cpp
@@ -3,22 +3,13 @@
 #include<iostream>
 using namespace std;
 
-void MoveString (vector<string> sourse, vector<string> destination) {
-
-}
-void test_MoveString () {
-    vector<string> test_a = {"a", "b", "c"};
-    vector<string> test_b = {"z"};
-    vector<string> test_c = {};
-    vector<string> test_d = {"a", "b", "c", "z"};
-    MoveString(test_a, test_b);
-    if (test_a == test_c && test_b == test_d) {
-        cout << "test_1: OK" << endl;
-    } else {
-        cout << "test_1: FAIL" << endl;
+void MoveString (vector<string>& sourse, vector<string>& destination) {
+    for (auto i : sourse) {
+        destination.push_back(i);
     }
+    sourse.clear();
 }
 
 int main () {
-    test_MoveString();
+    return 0;
 }
