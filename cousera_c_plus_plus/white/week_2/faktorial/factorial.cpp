148b07ad669386df24d6c82cd0a0df1b585814f8
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-20 09:40:50 +0300

    Add test_2-4: OK

--- cousera_c_plus_plus/white/week_2/faktorial/factorial.cpp
+++ cousera_c_plus_plus/white/week_2/faktorial/factorial.cpp
@@ -6,7 +6,7 @@ int Factorial (int x) {
     if (x <= 1) {
         return 1;
     }
-    answer = Factorial(x) * (x-1);
+    answer = Factorial(x-1) * (x);
     return answer;
 }
 
@@ -14,6 +14,23 @@ void test_Factorial () {
     
     if (Factorial(0) == 1) {
         cout << "test_1: OK" << endl;
+    } else {
+        cout << "test_1: FAIL" << endl;
+    }
+    if (Factorial(1) == 1) {
+        cout << "test_2: OK" << endl;
+    } else {
+        cout << "test_2: FAIL" << endl;
+    }
+    if (Factorial(5) == 120) {
+        cout << "test_3: OK" << endl;
+    } else {
+        cout << "test_3: FAIL" << endl;
+    }
+    if (Factorial(-2) == 1) {
+        cout << "test_4: OK" << endl;
+    } else {
+        cout << "test_4: FAIL" << endl;
     }
 }
 
