0f4d5ab839ea254d179a3635744781f338bc6692
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-20 10:07:03 +0300

    Add test_1-3

--- cousera_c_plus_plus/white/week_2/palindrom/palindrom.cpp
+++ cousera_c_plus_plus/white/week_2/palindrom/palindrom.cpp
@@ -1,10 +1,30 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
 bool IsPalindrom (string str) {
     return 0;
 }
 
+void test_IsPalindrom () {
+    if (IsPalindrom("madam") == true) {
+        cout << "test_1: OK" << endl;
+    } else {
+        cout << "test_1: FAIL" << endl;
+    }
+    if (IsPalindrom("gentleman") == false) {
+        cout << "test_2: OK" << endl;
+    } else {
+        cout << "test_2: FAIL" << endl;
+    }
+    if (IsPalindrom("X") == false) {
+        cout << "test_3: OK" << endl;
+    } else {
+        cout << "test_3: FAIL" << endl;
+    }
+}
+
 int main () {
+    test_IsPalindrom();
     return 0;
 }
