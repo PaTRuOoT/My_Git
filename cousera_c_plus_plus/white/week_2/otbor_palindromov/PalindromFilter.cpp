a16205acce7c267d5c1a52e15525f6204de142f9
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-20 11:18:38 +0300

    Add function IsPalindrom

--- cousera_c_plus_plus/white/week_2/otbor_palindromov/PalindromFilter.cpp
+++ cousera_c_plus_plus/white/week_2/otbor_palindromov/PalindromFilter.cpp
@@ -3,6 +3,18 @@
 #include<vector>
 using namespace std;
 
+bool IsPalindrom (string str) {
+    string inverted_string = "";
+    for (int i = str.size()-1; i >= 0; --i) {
+        inverted_string += str[i];
+    }
+    if (str == inverted_string) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 vector<string> PalindromFilter (vector<string> words, int minLength) {
     vector <string> result = {};
     return result;
