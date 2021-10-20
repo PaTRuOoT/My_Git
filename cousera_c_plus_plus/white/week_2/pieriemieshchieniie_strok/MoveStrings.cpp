3cd48ac19a7c428eb4d82af807f3381fcc541263
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-20 13:32:16 +0300

    Add test-1

--- cousera_c_plus_plus/white/week_2/pieriemieshchieniie_strok/MoveStrings.cpp
+++ cousera_c_plus_plus/white/week_2/pieriemieshchieniie_strok/MoveStrings.cpp
@@ -1,9 +1,24 @@
+#include<vector>
+#include<string>
+#include<iostream>
 using namespace std;
 
-void MoveString () {
+void MoveString (vector<string> sourse, vector<string> destination) {
 
+}
+void test_MoveString () {
+    vector<string> test_a = {"a", "b", "c"};
+    vector<string> test_b = {"z"};
+    vector<string> test_c = {};
+    vector<string> test_d = {"a", "b", "c", "z"};
+    MoveString(test_a, test_b);
+    if (test_a == test_c && test_b == test_d) {
+        cout << "test_1: OK" << endl;
+    } else {
+        cout << "test_1: FAIL" << endl;
+    }
 }
 
 int main () {
-    return 0;
+    test_MoveString();
 }
