cfe87d8307c2baa1cd816f3678f57f96db5f6b4f
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-20 09:50:15 +0300

    To SEND - Test_Coursera: OK

--- cousera_c_plus_plus/white/week_2/faktorial/factorial.cpp
+++ cousera_c_plus_plus/white/week_2/faktorial/factorial.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 int Factorial (int x) {
     int answer;
     if (x <= 1) {
@@ -8,32 +5,4 @@ int Factorial (int x) {
     }
     answer = Factorial(x-1) * (x);
     return answer;
-}
-
-void test_Factorial () {
-    
-    if (Factorial(0) == 1) {
-        cout << "test_1: OK" << endl;
-    } else {
-        cout << "test_1: FAIL" << endl;
-    }
-    if (Factorial(1) == 1) {
-        cout << "test_2: OK" << endl;
-    } else {
-        cout << "test_2: FAIL" << endl;
-    }
-    if (Factorial(5) == 120) {
-        cout << "test_3: OK" << endl;
-    } else {
-        cout << "test_3: FAIL" << endl;
-    }
-    if (Factorial(-2) == 1) {
-        cout << "test_4: OK" << endl;
-    } else {
-        cout << "test_4: FAIL" << endl;
-    }
-}
-
-int main () {
-    return 0;
 }
