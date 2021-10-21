e05729c0745eb06f505b327af3daa71ca21cf7af
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-21 14:01:01 +0300

    Add operation: COME

--- cousera_c_plus_plus/white/week_2/queue/queue.cpp
+++ cousera_c_plus_plus/white/week_2/queue/queue.cpp
@@ -6,12 +6,24 @@ using namespace std;
 
 int main () {
     vector<bool> people_in_queue = {};
+    string A;
+    int B;
     int N = 0; // количество операций
+    int size_vector = people_in_queue.size();
     cin >> N;
     for (int i = 0; i < N; ++i) {
-        string A;
-        int B;
         cin >> A >> B;
+        if (A == "COME") {
+            size_vector += B;
+            people_in_queue.resize(size_vector, false);
+        }
+        cout << "size: " << (people_in_queue.size()) << endl;
     }
+    // WORRY i - X
+    // QUIET i - X
+    // COME k - X
+    // COME -k - X
+    // WORRY_COUNT - X
+    
     return 0;
 }
