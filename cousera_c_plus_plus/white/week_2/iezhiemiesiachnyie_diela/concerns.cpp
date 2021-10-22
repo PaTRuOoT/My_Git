98a429c009d4302f7355cf4eaf8e3f8179ae5404
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-22 10:14:56 +0300

    New task: monthly_affairs

---
+++ cousera_c_plus_plus/white/week_2/iezhiemiesiachnyie_diela/concerns.cpp
@@ -0,0 +1,15 @@
+/*
+ADD i s - назначить дело s на день i
+DUMP i - вывести все дела, запланированные на день i
+NEXT - перейти к списку дел на месяц
+Q - число операций
+*/
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    return 0;
+}
