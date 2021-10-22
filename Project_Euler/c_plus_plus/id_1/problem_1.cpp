31cc0ae2d2a9a292f98d100777bc6e6833ecf270
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-22 11:51:28 +0300

    Add new task: Problem_1

---
+++ Project_Euler/c_plus_plus/id_1/problem_1.cpp
@@ -0,0 +1,21 @@
+/* 
+Problem_1:
+ENG:
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+RUS:
+Если выписать все натуральные числа меньше 10, кратные 3 или 5, 
+то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+
+Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+ */
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    return 0;
+}
