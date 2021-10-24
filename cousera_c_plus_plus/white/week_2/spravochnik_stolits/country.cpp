7ce702d0f82be042bc1a4c14f15d6fd9a121143a
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-24 22:19:23 +0300

    Add command 'CHANGE_CAPITAL, RENAME, ABOUT, DUMP'

--- cousera_c_plus_plus/white/week_2/spravochnik_stolits/country.cpp
+++ cousera_c_plus_plus/white/week_2/spravochnik_stolits/country.cpp
@@ -17,22 +17,42 @@ DUMP — вывод столиц всех стран.
 
 using namespace std;
 
+void PrintMap (const map<string, string>& m) {
+    for (const auto& i : m) {
+        cout << i.first << "/" << i.second;
+    }
+    cout << endl;
+
+}
+
 int main () {
+    map<string, string> country_with_capital;
     int Q = 0; // количество запросов
     cin >> Q;
     for (int i = 0; i < Q; i++) {
         string A;
         cin >> A;
         if (A == "CHANGE_CAPITAL") {
+            string B, C;
+            cin >> B >> C;
+            country_with_capital[B] = C; 
             // изменение сталицы country на new_capital
             // или добавление такой страны с такой сталицей
             // если её раньше не было
         } else if (A == "RENAME") {
+            string B, C;
+            cin >> B >> C;
+            country_with_capital[C] = country_with_capital[B];
+            country_with_capital.erase(B);
             // переименование страны из old_country_name
             // в new_country_name
         } else if (A == "ABOUT") {
+            string B;
+            cin >> B;
+            cout <<  country_with_capital[B] << endl;
             // вывод сталицы страны country
         } else if (A == "DUMP") {
+            PrintMap(country_with_capital);
             // вывод столиц всех стран
         }
     }
