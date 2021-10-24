bb65d0f8698f57dbcce46cfa561d74b9090e2a06
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-24 18:02:42 +0300

    Add command 'ADD'

--- cousera_c_plus_plus/white/week_2/iezhiemiesiachnyie_diela/concerns.cpp
+++ cousera_c_plus_plus/white/week_2/iezhiemiesiachnyie_diela/concerns.cpp
@@ -14,9 +14,20 @@ using namespace std;
 // вектор количества дней по месяцам (31, 30 и тд)
 // вектор со списком дел ({месяц 1, 2 и тд}, {день}, {список дел})
 // ф-ция вывода списка дел в конкретный день с аргументом день
+// ф-ция инициализации вектора на месяц month
 
+void PrintToDoListInDay (const vector<vector<string>>& v, int day) {
+    for (int i=0; i < v[day-1].size(); ++i) {
+        cout << v[day-1][i] << " ";
+    }    
+
+}
 
 int main () {
+    int month = 1; // текущий месяц
+    vector<int> days_in_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    vector<vector<string>> to_do_list = {};
+    to_do_list.resize(days_in_month[month-1], {});
     int N = 0; // количество операций
     cin >> N;
     for (int i = 0; i < N; ++i) {
@@ -29,7 +40,10 @@ int main () {
             cout << "DUMP" << endl;
             // функция вывода дел
         } else if (A == "ADD") {
-            cout << "ADD" << endl;
+            int i;
+            string s;
+            cin >> s >> i;
+            to_do_list[i-1].push_back(s);
             // добавить дело s на день i текущего месяца
         }
     }
