a7d3942a6a8ed2210b930dab8059a02e57ee5868
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-24 21:51:35 +0300

    to SEND - Tesk_Cousera: OK

--- cousera_c_plus_plus/white/week_2/anaghrammy/anagrams.cpp
+++ cousera_c_plus_plus/white/week_2/anaghrammy/anagrams.cpp
@@ -1,5 +1,28 @@
+#include <map>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
+map<char, int> BuildCharCounter (const string& temp_str) {
+    map<char, int> result_map;
+    for (const char& i : temp_str) {
+        ++result_map[i];
+    } 
+    return result_map;
+}
+
 int main () {
+    int N; // количество операций
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        string word_1, word_2;
+        cin >> word_1 >> word_2;
+        if (BuildCharCounter(word_1) == BuildCharCounter(word_2)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
