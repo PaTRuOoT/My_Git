56729d22cdb94f885a81fd6566738b1c12966571
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-25 19:37:22 +0300

    upd

--- cousera_c_plus_plus/white/week_2/avtobusnyie_ostanovki_1/bus.cpp
+++ cousera_c_plus_plus/white/week_2/avtobusnyie_ostanovki_1/bus.cpp
@@ -19,26 +19,18 @@ void PrintMap (const map<int, vector<string>>& m) {
     }
 }
 
-void Print_BUSES_FOR_STOP (const string& stop, const map<int, vector<string>>& m) {
+string Print_BUSES_FOR_STOP (const string& stop, const map<int, vector<string>>& m) {
+    string str = "";
     int count_stop = 0;
     for (const auto& i : m) {
         for (int j = 1; j < i.second.size(); j++) {
             if (i.second[j] == stop) {
-                cout << i.second[0] << " ";
-                count_stop++;
+                str += i.second[0];
+                str += " ";
             }
         }
     }
-    if (count_stop == 0) {
-        cout << "No stop" << endl;
-    } else {
-        cout << endl;
-    }
-    // No stop
-    // No buses
-    // return: vector ? string
-    // string == bus. Stop stop: no interchange
-    // else cout << string
+    return str;
 }
 
 int main() {
@@ -61,12 +53,43 @@ int main() {
             }
             count_bus_routes++;
         } else if (inquiry == "BUSES_FOR_STOP") {
-            string stop;
+            string stop, str;
             cin >> stop;
-            Print_BUSES_FOR_STOP(stop, bus_routes);
+            str = Print_BUSES_FOR_STOP(stop, bus_routes);
+            if (str == "") {
+                cout << "No stop" << endl;
+            } else {
+                cout << str << endl;
+            }
         } else if (inquiry == "STOPS_FOR_BUS") {
-            string bus;
+            string bus, str;
             cin >> bus;
+            int count_bus = 0;
+            for (const auto& i : bus_routes) {
+                if (i.second[0] == bus) {
+                    count_bus++;
+                }
+            }
+            if (count_bus == 0) {
+                cout << "No bus" << endl;
+            } else {
+                for (const auto& i : bus_routes) { 
+                    if (i.second[0] != bus) {
+                        continue;
+                    } else {
+                        for (int j=1; j<i.second.size(); j++) {
+                            str = Print_BUSES_FOR_STOP(i.second[j], bus_routes);
+                            if (str == (bus + " ")) {
+                                cout << "Stop " << i.second[j] << ": no interchange";
+                                cout << endl;
+                                continue;
+                            } else {
+                                cout << "Stop " << i.second[j] << ": " << str << endl;
+                            }
+                        }
+                    }
+                }
+            }
         } else if (inquiry == "ALL_BUSES") {
             PrintMap(bus_routes);
         }
