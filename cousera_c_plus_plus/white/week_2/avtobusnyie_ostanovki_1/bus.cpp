157940a19e5f9407bf2fb70d9ed318d739055ecd
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-25 11:31:45 +0300

    Add empty command 'NEW_BUS, BUSES_FOR_STOP, STOPS_FOR_BUS, ALL_BUSES'

---
+++ cousera_c_plus_plus/white/week_2/avtobusnyie_ostanovki_1/bus.cpp
@@ -0,0 +1,27 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int Q = 0; // количество запросов
+    cin >> Q;
+    for (int i = 0; i < Q; i++) {
+        string inquiry;
+        cin >> inquiry;
+        if (inquiry == "NEW_BUS") {
+
+        } else if (inquiry == "BUSES_FOR_STOP") {
+
+        } else if (inquiry == "STOPS_FOR_BUS ") {
+
+        } else if (inquiry == "ALL_BUSES") {
+
+        }
+     }
+    /* code */
+    return 0;
+}
+
