11387a294a9e87f15f38d7f2e72b4c24f54ccfc0
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-27 12:44:19 +0300

    to SEND - Tesk_Cousera: OK

--- cousera_c_plus_plus/white/week_2/avtobusnyie_ostanovki_3/bus3.cpp
+++ cousera_c_plus_plus/white/week_2/avtobusnyie_ostanovki_3/bus3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <vector>
+
+#include <set>
 #include <map>
 
 using namespace std;
@@ -8,8 +9,8 @@ int main () {
     int Q = 0; // общее количество запросов
     cin >> Q; // считываем общее количество запросов
     int count_stop = 1; // количество уникальных остановок в map
-    vector<string> v_stop; // вектор для добавления остановок в map
-    map<vector<string>, int> bus_stop; // map хранящая остановки
+    set<string> v_stop; // множество для добавления остановок в map
+    map<set<string>, int> bus_stop; // map хранящая остановки
     for (int q = 0; q < Q; q++) {
         // проходим циклом от 0 до Q, равному количеству запросов
         int N = 0; // переменная для считывания количество остановок в запросе
@@ -19,7 +20,7 @@ int main () {
             // проходим циклом от 0 до N, равному количеству остановок в запросе
             string tmp_stop; // переменная для считывания названия остановки
             cin >> tmp_stop; // считываем название остановки
-            v_stop.push_back(tmp_stop); // добавляем остановку во временный вектор
+            v_stop.insert(tmp_stop); // добавляем остановку во временный сет
         }
         if (bus_stop.count(v_stop) != 0) {
             // проверяем условем
@@ -47,7 +48,7 @@ int main () {
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include <set>
 #include <map>
 
 using namespace std;
@@ -56,25 +57,21 @@ int main() {
   int q;
   cin >> q;
 
-  map<vector<string>, int> buses;
+  map<set<string>, int> buses;
 
   for (int i = 0; i < q; ++i) {
     int n;
     cin >> n;
-    vector<string> stops(n);
-    for (string& stop : stops) {
+    set<string> stops;
+    for (int j = 0; j < n; ++j) {
+      string stop;
       cin >> stop;
+      stops.insert(stop);
     }
-    
-    // проверяем, не существует ли уже маршрут с таким набором остановок
     if (buses.count(stops) == 0) {
-      
-      // если не существует, нужно сохранить новый маршрут;
-      // его номер на единицу больше текущего количества маршрутов
       const int new_number = buses.size() + 1;
       buses[stops] = new_number;
       cout << "New bus " << new_number << endl;
-      
     } else {
       cout << "Already exists for " << buses[stops] << endl;
     }
@@ -82,4 +79,6 @@ int main() {
 
   return 0;
 }
+
+
  */
