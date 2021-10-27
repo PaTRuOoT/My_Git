94259e746bc976ad506fa2cc2ca96c85373295ab
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-27 11:47:40 +0300

    to SEND - Tesk_Cousera: OK

--- cousera_c_plus_plus/white/week_2/kolichiestvo_unikalnykh_strok/str.cpp
+++ cousera_c_plus_plus/white/week_2/kolichiestvo_unikalnykh_strok/str.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+#include <string>
+#include <set>
+
 using namespace std;
 
-int maint () {
+int main () {
+    int N = 0; // количество уникальных строк
+    cin >> N;
+    set<string> many_unique_strings;
+    for (int i=0; i < N; i++) {
+        string tmp_str;
+        cin >> tmp_str;
+        many_unique_strings.insert(tmp_str);
+    }
+    cout << many_unique_strings.size();
     return 0;
 }
