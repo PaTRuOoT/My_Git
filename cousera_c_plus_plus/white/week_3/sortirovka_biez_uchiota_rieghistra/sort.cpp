3e0171c9adcf34e36dc65039f8b8bd3bcd5aa2df
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-27 21:25:14 +0300

    to SEND - Tesk_Cousera: OK

--- cousera_c_plus_plus/white/week_3/sortirovka_biez_uchiota_rieghistra/sort.cpp
+++ cousera_c_plus_plus/white/week_3/sortirovka_biez_uchiota_rieghistra/sort.cpp
@@ -1,5 +1,32 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool modulo_string_comparison (string x, string y) {
+    for (auto& i : x) {
+        i = tolower(i);
+    }
+    for (auto& i : y) {
+        i = tolower(i);
+    }
+    return x < y;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    vector<string> v_string;
+    int N = 0;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        string str;
+        cin >> str;
+        v_string.push_back(str);
+    }
+    sort(begin(v_string), end(v_string), modulo_string_comparison);
+    for (const auto& i : v_string) {
+        cout << i << " ";
+    }
     return 0;
 }
