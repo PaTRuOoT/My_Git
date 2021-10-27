04c91b34217f2b3ee623982e0d1a88bf3875ea62
PaTRuOoT <roma_mishenko@mail.ru>
2021-10-27 20:59:00 +0300

    to SEND - Tesk_Cousera: OK

--- cousera_c_plus_plus/white/week_3/sort_number_for_modul/sort.cpp
+++ cousera_c_plus_plus/white/week_3/sort_number_for_modul/sort.cpp
@@ -1,5 +1,24 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int N = 0;
+    cin >> N;
+    vector<int> vector;
+    for (int i = 0; i < N; i++) {    
+        int number = 0;
+        cin >> number;
+        vector.push_back(number);
+    }
+    sort(begin(vector), end(vector), [](int x, int y) {return abs(x) < abs(y);});
+    // на вход два числа x и y
+    // необходимо получить true, если |x| < |y|
+    // взятие числа по модулю функция abs()
+    for (const auto& i : vector) {
+        cout << i << " ";
+    }
     return 0;
 }
