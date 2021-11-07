f7cdf5952ce3cd648d1bbca0aca6f06326b7dec2
PaTRuOoT <roma_mishenko@mail.ru>
2021-11-07 15:24:16 +0300

    Add solve by coursera

--- cousera_c_plus_plus/white/week_3/imiena_i_familii_1/names1.cpp
+++ cousera_c_plus_plus/white/week_3/imiena_i_familii_1/names1.cpp
@@ -1,47 +1,61 @@
-#include <iostream>
-#include <string>
 #include <map>
-#include <vector>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
-struct FullName {
-    string FirstName;
-    string LastName;
-};
+// если имя неизвестно, возвращает пустую строку
+string FindNameByYear(const map<int, string>& names, int year) {
+  string name;  // изначально имя неизвестно
+  
+  // перебираем всю историю по возрастанию ключа словаря, то есть в хронологическом порядке
+  for (const auto& item : names) {
+    // если очередной год не больше данного, обновляем имя
+    if (item.first <= year) {
+      name = item.second;
+    } else {
+      // иначе пора остановиться, так как эта запись и все последующие относятся к будущему
+      break;
+    }
+  }
+  
+  return name;
+}
 
 class Person {
 public:
   void ChangeFirstName(int year, const string& first_name) {
-        if (person_name.count(year) == 0) person_name[year].LastName = "";
-        person_name[year].FirstName = first_name;
+    first_names[year] = first_name;
+  }
+  void ChangeLastName(int year, const string& last_name) {
+    last_names[year] = last_name;
+  }
+  string GetFullName(int year) {
+    // получаем имя и фамилию по состоянию на год year
+    const string first_name = FindNameByYear(first_names, year);
+    const string last_name = FindNameByYear(last_names, year);
+    
+    // если и имя, и фамилия неизвестны
+    if (first_name.empty() && last_name.empty()) {
+      return "Incognito";
+    
+    // если неизвестно только имя
+    } else if (first_name.empty()) {
+      return last_name + " with unknown first name";
+      
+    // если неизвестна только фамилия
+    } else if (last_name.empty()) {
+      return first_name + " with unknown last name";
+      
+    // если известны и имя, и фамилия
+    } else {
+      return first_name + " " + last_name;
     }
-    void ChangeLastName(int year, const string& last_name) {
-        if (person_name.count(year) == 0) person_name[year].FirstName = "";
-        person_name[year].LastName = last_name;
-    }
-    string GetFullName(int year) {
-        if (person_name.size() == 0) {
-            return "Incognito";
-        }
-        else {
-            for (const auto& i : person_name) {
-                if (year < i.first) return "Incognito";
-                break;
-            }
-        }
-        string nm = "";
-        string sn = "";
-        for (const auto& i : person_name) {
-            if (i.first <= year && i.second.LastName != "") sn = i.second.LastName;
-            if (i.first <= year && i.second.FirstName != "") nm = i.second.FirstName;
-        }
-        if(nm == "") return sn + " with unknown first name";
-        else if(sn == "") return nm + " with unknown last name";
-        else return nm + " " + sn;
-    }
+  }
+
 private:
-    map<int, FullName> person_name;
+  map<int, string> first_names;
+  map<int, string> last_names;
 };
 
 int main() {
