70e110c3218749660b8d4b253aec5e27b6861901
PaTRuOoT <roma_mishenko@mail.ru>
2021-11-07 13:54:08 +0300

    to SEND - Tesk_Cousera: OK

--- cousera_c_plus_plus/white/week_3/otsortirovannyie_stroki/sorted.cpp
+++ cousera_c_plus_plus/white/week_3/otsortirovannyie_stroki/sorted.cpp
@@ -1,5 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
 using namespace std;
 
-int main () {
-    
+
+class SortedStrings {
+public:
+  void AddString(const string& s) {
+      strings.push_back(s);
+    // добавить строку s в набор
+  }
+  vector<string> GetSortedStrings() {
+      sort(begin(strings), end(strings));
+      return strings;
+    // получить набор из всех добавленных строк в отсортированном порядке
+  }
+private:
+    vector<string> strings;
+  // приватные поля
+};
+
+
+
+void PrintSortedStrings(SortedStrings& strings) {
+  for (const string& s : strings.GetSortedStrings()) {
+    cout << s << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+  SortedStrings strings;
+  
+  strings.AddString("first");
+  strings.AddString("third");
+  strings.AddString("second");
+  PrintSortedStrings(strings);
+  
+  strings.AddString("second");
+  PrintSortedStrings(strings);
+  
+  return 0;
 }
