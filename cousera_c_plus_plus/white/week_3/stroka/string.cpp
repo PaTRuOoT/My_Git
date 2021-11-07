d02022d0aaa551694a4cb8d69fa8b33055d9c649
PaTRuOoT <roma_mishenko@mail.ru>
2021-11-07 19:17:33 +0300

    to SEND - Tesk_Cousera: OK

--- cousera_c_plus_plus/white/week_3/stroka/string.cpp
+++ cousera_c_plus_plus/white/week_3/stroka/string.cpp
@@ -1,5 +1,40 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
-int main () {
-    
+class ReversibleString {
+    public:
+        ReversibleString() {}
+        ReversibleString(const string& t_str) {
+            str = t_str;
+        }
+        void Reverse () {
+            reverse(begin(str), end(str));
+        }
+        string ToString () const {
+            return str;
+        }
+
+    private:
+        string str;
+};
+
+
+int main() {
+    ReversibleString s("live");
+    s.Reverse();
+
+  cout << s.ToString() << endl;
+  
+  s.Reverse();
+  const ReversibleString& s_ref = s;
+  string tmp = s_ref.ToString();
+  cout << tmp << endl;
+  
+  ReversibleString empty;
+  cout << '"' << empty.ToString() << '"' << endl;
+  
+  return 0;
 }
